df152cd72f73363f59dfcb14abf6caaf15bc44cb
Vikings <43942467+city8208@users.noreply.github.com>
2020-03-26 17:15:07 +0800

    fix gif creat function 20200326

--- index.js
+++ index.js
@@ -1,53 +1,55 @@
-var __awesome_qr_base_path = "awesome-qr"; 
-function imgQRcodeCovert(QRcodeUrl){
-    if(nowLoadFileType == 'gif'){ ////GIF用
+var __awesome_qr_base_path = "js"; 
+function imgQRcodeCovert(QRcodeUrl,PicLoadFile,PicLoadFileType){
+    if(PicLoadFileType == 'gif'){ ////GIF用
         require([__awesome_qr_base_path + '/awesome-qr'], function (AwesomeQR) {
             AwesomeQR.create({
                 text: QRcodeUrl,
                 size: 800,
                 margin: 20,
-                gifBackground: nowLoadFile, 
-                bindElement: 'qrcodeimage'
+                gifBackground: PicLoadFile, 
+                bindElement: 'example_qrcodeimage'
             });
         });  
+        
     }else{  ///JPG用
         require([__awesome_qr_base_path + '/awesome-qr'], function (AwesomeQR) {
             AwesomeQR.create({
                 text: QRcodeUrl,
                 size: 800,
-                backgroundImage: document.getElementById("uploadimage"),
+                backgroundImage: document.getElementById("example_uploadimage"),
                 margin: 20,
-                bindElement: 'qrcodeimage'
+                bindElement: 'example_qrcodeimage'
             });
         });  
     }
 }
 
 ////20200304讀取上傳圖片 || Folder檔案
-var nowLoadFile ="";
-var nowLoadFileType ="";
 function filecheck(FileURL){
     ts('.snackbar').snackbar({
         content: '讀取中...'
     });
-    ///////////20200319新增 GIF 檔案內容(將Blob轉為ArrayBuffer)
-    nowLoadFile = FileURL.files[0];
-    let reader = new FileReader();
-    reader.readAsArrayBuffer(nowLoadFile);
-    reader.onload = function () {
-        nowLoadFile = this.result;
-        nowLoadFile = new Uint8Array(nowLoadFile);
-    }
-    //////////////
-
-    nowLoadFileType = FileURL.value.split('.').pop();
+    let FileType = FileURL.value.split('.').pop();
+    let LoadFile = FileURL.files[0];
+    document.getElementById('example_qrcodeimage').src = 'loading.gif';
     ///alert(FileURL.value.split('.').pop());
-    if(nowLoadFileType == 'png' || nowLoadFileType == 'jpg' || nowLoadFileType == 'gif' || nowLoadFileType == 'jpeg' ){////檢測檔案格式(圖片)
+    if(FileType == 'png' || FileType == 'jpg' || FileType == 'jpeg' ){////檢測檔案格式(圖片)
         //alert(FileURL.value.split('.').pop());
-        document.getElementById("uploadimage").src = window.URL.createObjectURL(FileURL.files[0]);
-        document.getElementById('qrcodeimage').src = 'loading.gif';
-        imgQRcodeCovert(document.getElementById("QRcode_Url").value);
+        document.getElementById("example_uploadimage").src = window.URL.createObjectURL(FileURL.files[0]);
+        imgQRcodeCovert(document.getElementById("example_QRcode_Url").value,FileURL.files[0],FileType);
+    }else if(FileType == 'gif' ){
+        document.getElementById("example_uploadimage").src = window.URL.createObjectURL(FileURL.files[0]);
+        ///////////20200319新增 GIF 檔案內容(將Blob轉為ArrayBuffer)
+        let gitFile = '';
+        let reader = new FileReader();
+        reader.readAsArrayBuffer(LoadFile);
+        reader.onload = function () {
+            gitFile = this.result;
+            gitFile = new Uint8Array(gitFile);            
+            imgQRcodeCovert(document.getElementById("example_QRcode_Url").value,gitFile,FileType);
+        }
+        //////////////
     }else{
-        alert("尚未支援"+nowLoadFileType+"格式。");
+        alert("尚未支援"+FileType+"格式。");
     }
 }
